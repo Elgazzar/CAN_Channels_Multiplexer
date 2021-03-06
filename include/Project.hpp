8147c363c3cb5df89f36736d4853f00d04e91de0
m7moudelgazzar <m7moudelgazzar@gmail.com>
2021-03-07 00:58:25 +0200

    Initial implementation for serial communication
    Initial implementation for serial communication

--- include/Project.hpp
+++ include/Project.hpp
@@ -3,10 +3,18 @@
 
 #include "Mux_Handler/Mux_Handler.hpp"
 
+/*Array IDX*/
+#define IDX_0 0
+#define IDX_1 1
+#define IDX_2 2
+/*define string max length*/
+#define Max_Length 3
+
 /*Define channel names with ECU Channels names*/
 #define Veh_Channel Channel0
 #define MRR_Channel Channel1
 #define SRR_F_Channel Channel2
 #define SRR_R_Channel Channel3
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,16 @@
 Channel_Cfg_T ECU1;
 Channel_Cfg_T ECU2;
 
+/*Serial Configuration*/
+char incomingByte;
+char incomingByte_Idx = 0;
+char received_data[Max_Length];
+
+
 void setup() {
   /*Call IoHwAbs init function*/
   IoHwAb_Init();
+  Serial.begin(9600);
   ECU1.Selection_0 = S0;
   ECU1.Selection_1 = S1;
   ECU2.Selection_0 = S2;
@@ -18,4 +25,51 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   SelectChannel(Veh_Channel,ECU1);
+  if (Serial.available() > 0)
+  {
+    incomingByte = Serial.read();
+    received_data[incomingByte_Idx]=incomingByte;
+    incomingByte_Idx++;
+    if ( (received_data[0] == 'V') && (received_data[1] == '1') )
+    {
+      Serial.println("Veh Channel Connected for ECU1");
+    }
+    else if ( (received_data[IDX_0] == 'V') && (received_data[IDX_1] == '2') )
+    {
+      Serial.println("Veh Channel Connected for ECU2");
+    }
+    else if ( (received_data[IDX_0] == 'M') && (received_data[IDX_1] == '1') )
+    {
+      Serial.println("MRR Channel Connected for ECU1");
+    }
+    else if ( (received_data[IDX_0] == 'M') && (received_data[IDX_1] == '2') )
+    {
+      Serial.println("MRR Channel Connected for ECU2");
+    }
+    else if ( (received_data[IDX_0] == 'S') && (received_data[IDX_1] == '1') )
+    {
+      Serial.println("SRR Front Channel Connected for ECU1");
+    }
+    else if ( (received_data[IDX_0] == 'S') && (received_data[IDX_1] == '2') )
+    {
+      Serial.println("SRR Front Channel Connected for ECU2");
+    }
+    else if ( (received_data[IDX_0] == 'R') && (received_data[IDX_1] == '1') )
+    {
+      Serial.println("SRR Rear Channel Connected for ECU1");
+    }
+    else if ( (received_data[IDX_0] == 'R') && (received_data[IDX_1] == '2') )
+    {
+      Serial.println("SRR Rear Channel Connected for ECU2");
+    }
+    else
+    {
+      //Serial.println("Veh Channel Connected for ECU1");
+      //Serial.println("Veh Channel Connected for ECU2");
+    }
+    if (incomingByte == '\n')
+    {
+      incomingByte_Idx = 0;
+    }
+  }
 }
