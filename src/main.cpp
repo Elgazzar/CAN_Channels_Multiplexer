4f25474063e32713cedb1e8fa722c246c1a2efe1
m7moudelgazzar <m7moudelgazzar@gmail.com>
2021-03-06 21:10:17 +0200

    Add initial code
    Add initial code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include "IOHwabs/IOHwabs.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
+  /*Call IOHwabs init function*/
+  IOHwabs_init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  R_IOHwabs_5ms();
 }
